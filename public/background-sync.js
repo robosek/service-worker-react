09444c304712531b7984f3e889b73a41444e61ec
Robert Sek <robert.sek@powel.no>
2017-05-18 14:48:00 +0200

    Add simple background-sync feature

---
+++ public/background-sync.js
@@ -0,0 +1,24 @@
+﻿//Simple background sync
+self.addEventListener('sync', function (event) {
+    console.log('firing: sync');
+    if (event.tag === 'myFirstSync') {
+        console.log('myFirstSync event fired');
+        event.waitUntil(fetchRandomImage());
+    }
+});
+
+function fetchRandomImage() {
+    //https://unsplash.it/200/300/?random
+    console.log('fetching random Image');
+    fetch('https://unsplash.it/200/300/?random')
+        .then((response) => {
+            console.log(response);
+            return response;
+        })
+        .then((text) => {
+            console.log('Request successfull ' + text);
+        })
+        .catch((error) => {
+            console.log('Request failed ' + error);
+        });
+}
--- sw-precache-config.js
+++ sw-precache-config.js
@@ -5,6 +5,9 @@ module.exports = {
     'build/manifest.json',
     'build/static/**/!(*map*)'
   ],
+  importScripts: [
+      'background-sync.js'
+  ],
   dontCacheBustUrlsMatching: /\.\w{8}\./,
   swFilePath: 'build/service-worker.js'
 };
